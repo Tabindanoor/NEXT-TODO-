9c45e6c634cec22170d1ac5e176721129bf0a44a
Tabindanoor <tabindanoor415@gmail.com>
2024-12-02 21:38:53 +0500

    commit

--- src/app/localstorage/page.tsx
+++ src/app/localstorage/page.tsx
@@ -1,10 +1,8 @@
-import { CustomKanban } from "@/components/CustomKanban"
 import LocalStorage from "../../components/LocalStorage"
 
 const page = () => {
   return (
     <div>
-      {/* <CustomKanban/> */}
         <LocalStorage/>
     </div>
   )
