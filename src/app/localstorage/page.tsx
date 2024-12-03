8fb22182e267f070278bd851be345ec337522834
Tabindanoor <tabindanoor415@gmail.com>
2024-12-03 17:40:21 +0500

    commit

--- src/app/localstorage/page.tsx
+++ src/app/localstorage/page.tsx
@@ -3,6 +3,7 @@ import LocalStorage from "../../components/LocalStorage"
 const page = () => {
   return (
     <div>
+        <p>this is the local storage todo </p>
         <LocalStorage/>
     </div>
   )
