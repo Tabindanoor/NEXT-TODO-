fa81db4d87c7c910d534b9de39f91ea49c6dc271
Tabindanoor <tabindanoor415@gmail.com>
2024-12-18 21:26:12 +0500

    commit

--- src/components/CustomKanban.tsx
+++ src/components/CustomKanban.tsx
@@ -35,10 +35,7 @@ interface AddCardProps {
 export const CustomKanban: React.FC = () => {
   return (
     <div className="h-screen w-full bg-neutral-900 overflow-x-hidden overflow-y-hidden text-neutral-50">
-      <Board />
-      <Board />
-      <Board />
-    </div>
+      <Board />    </div>
   );
 };
 
