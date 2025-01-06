59e0235c9b9caa6dea1ec78b3da19ae67d9445b1
Tabindanoor <tabindanoor415@gmail.com>
2025-01-06 22:24:37 +0500

    msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import CustomKanban from "../components/CustomKanban";
 
 export default function Home() {
   return (
-    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     <div>   
     <CustomKanban/>
     </div>
   );
 }
+
+
+
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
